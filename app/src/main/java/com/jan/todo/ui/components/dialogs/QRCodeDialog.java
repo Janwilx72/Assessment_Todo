de9116e3499b1de706b371fea458151e008692bc
Janwilx72 <102793000+Janwilx72@users.noreply.github.com>
2024-01-12 14:28:42 +0200

    Develop (#13)
    * Added dependencies for Room and Dagger2.
    
    * Feature/implement room database (#2)
    
    * Added Database files to create TodoItemEntity and SQLite database
    
    * Added Repository class to access Todo table
    
    * Created Todo Activity And Viewmodel (#3)
    
    * Created Dagger Files for Dependency injection and added Adapters for TodoItems in Activity (#4)
    
    * Created Dialog to Create an Entry and added required image resources (#5)
    
    * Feature/update delete entry (#6)
    
    * Created Dialog to Create an Entry and added required image resources
    
    * Created dialog to update and delete existing entries
    
    * Feature/sort by word occurence (#7)
    
    * Added feature to sort by times a word occurs in an Entry
    
    * Feature/qr code scanning and creation (#8)
    
    * Fixed some syntax issues
    
    * Added functionality to convert Todo item to QRCode
    
    * Added functionality to scan an Entry and Add it to device
    
    * Added error handling for QR code scanning
    
    * Updated README.md and added Dummy data (#9)
    
    * Creating Hash from Payload before and after sharing to ensure it matches (#12)

--- app/src/main/java/com/jan/todo/ui/components/dialogs/QRCodeDialog.java
+++ app/src/main/java/com/jan/todo/ui/components/dialogs/QRCodeDialog.java
@@ -8,20 +8,27 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
 import com.jan.todo.R;
+import com.jan.todo.core.database.entities.TodoItemEntity;
+import com.jan.todo.core.helpers.QRCodeHelper;
+
+import java.security.NoSuchAlgorithmException;
 
 public class QRCodeDialog extends DialogFragment
 {
     private final Bitmap _bitmap;
+    private final TodoItemEntity _entity;
 
-    public QRCodeDialog(final Bitmap bitmap)
+    public QRCodeDialog(final Bitmap bitmap, final TodoItemEntity entity)
     {
         _bitmap = bitmap;
+        _entity = entity;
     }
 
     @NonNull
@@ -48,5 +55,19 @@ public class QRCodeDialog extends DialogFragment
 
         final ImageView ivQRCode = layoutView.findViewById(R.id.ivQRCode);
         ivQRCode.setImageBitmap(_bitmap);
+
+        final TextView tvHash = layoutView.findViewById(R.id.tvHash);
+        QRCodeHelper qrCodeHelper = new QRCodeHelper();
+        try
+        {
+            final String hash = qrCodeHelper.generateHashFromString(_entity.toString());
+            tvHash.setText(hash);
+        }
+        catch (NoSuchAlgorithmException e)
+        {
+            tvHash.setText("Error generating Hash");
+        }
+
+
     }
 }
